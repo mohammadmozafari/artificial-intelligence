5a985929c02b119deac7d424219d056e5c22491e
mohammad <mozafari.mmd@gmail.com>
2019-12-11 21:06:29 +0330

    added __rotate method

--- p1a.py
+++ p1a.py
@@ -10,4 +10,11 @@ class Rubik:
         for i in range(len(self.face)):
             result += 'face ' + str(i + 1) + ': ' + ', '.join([str(elem) for elem in self.face[i]]) + '\n'
         return result
+
+    def __rotate(self, idx):
+        rotated_list = []
+        for (x, y) in idx:
+            rotated_list.append(self.face[x][y])
+        rotated_list = rotated_list[1:] + rotated_list[:1]
+        return rotated_list
         

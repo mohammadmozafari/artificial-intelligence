8c35ea37847e739e2febf222211d71d9df761d3d
mohammad <mozafari.mmd@gmail.com>
2019-12-16 00:08:18 +0330

    rotate method combines rotate_surface & rotate_perimeter

--- midterm_project/p1a.py
+++ midterm_project/p1a.py
@@ -1,13 +1,16 @@
 import copy
 
 class Rubik:
+    """
+    This class represents one instance of rubik puzzle.
+    """
 
     def __init__(self, initial_state):
         self.num_faces = len(initial_state)
         self.dim = len(initial_state[0])
         self.state = copy.deepcopy(initial_state)
     
-    # def move(self, side, num):
+    # def move(self, side, count):
     #     if side == 0:
     #         pass
     #     elif side == 1:
@@ -17,23 +20,29 @@ class Rubik:
     #     elif side == 3:
     #         pass
     #     elif side == 4:
-    #         rotate((5, 1, 1), (5, 0, 1), (4, 1, 1),
-    #                (4, 0, 1), (2, 1, 1), (2, 0, 1), (0, 1, 1), (0, 1, 1), 4)
+    #         rotate([(5, 1, 1), (5, 0, 1), (4, 1, 1), (4, 0, 1), (2, 1, 1), (2, 0, 1), (0, 1, 1), (0, 1, 1)], 4, count)
     #     elif side == 5:
     #         pass
 
-    # def rotate(self, idx, num):
-    #     new_rubik = copy.deepcopy(rubik)
+    def rotate(self, idx, num, count):
+        """
+        Input
+        - idx: address of the pieces to be rotated
+        - num: number of the surface to be rotated
+        - count: number of times to perform rotation
+        """
+        new_rubik = copy.deepcopy(self.state)
+        self.rotate_surface(new_rubik, num, count)
+        self.rotate_perimeter(new_rubik, idx, 2 * count)
+        return Rubik(new_rubik)
 
-    #     pass
-
-    """
-    Input
-    - rubik: state of the rubik
-    - surface: number of the surface to be rotated
-    - count: number of times to rotate the surface
-    """
     def rotate_surface(self, rubik, surface_num, count):
+        """
+        Input
+        - rubik: state of the rubik
+        - surface: number of the surface to be rotated
+        - count: number of times to rotate the surface
+        """
         N = self.dim
         for x in range(count):
             for i in range(N // 2):
@@ -44,13 +53,13 @@ class Rubik:
                     rubik[surface_num][N - 1 - i][N - 1 - j] = rubik[surface_num][j][N - 1 - i]
                     rubik[surface_num][j][N - 1 - i] = temp
 
-    """
-    Input
-    - rubik: state of the rubik cube
-    - idx: address of the pieces to be rotated
-    - count: number of times to rotate
-    """
     def rotate_perimeter(self, rubik, idx, count):
+        """
+        Input
+        - rubik: state of the rubik cube
+        - idx: address of the pieces to be rotated
+        - count: number of times to rotate
+        """
         rotated_list = []
         for (x, y, z) in idx:
             rotated_list.append(rubik[x][y][z])

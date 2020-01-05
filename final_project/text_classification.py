ccc28fef3dd0125c230cb43f66af37ac01cb9112
mohammad <mozafari.mmd@gmail.com>
2020-01-05 21:53:33 +0330

    build_model method implemented

--- final_project/text_classification.py
+++ final_project/text_classification.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import json
 
 # TODO: linux space character used
 
@@ -110,9 +111,43 @@ class Model:
         pd.DataFrame.to_csv(dataframe, csv_file, index=False, encoding='ansi')
         print('writing done')
 
-    # @staticmethod
-    # def get_titles():
-    #     return ['اقتصاد', 'سیاست']
+    def build_model(self, text_file, say=True):
+        """
+        This method builds the model which we use later to compute probability
+        of a class for a text. (Text Classification)
+        """
+        model = {}
+        with open(text_file) as f:
+            lines = f.readlines()
+            for (i, line) in enumerate(lines):
+                if say: print('reading line', i + 1)
+                title, text = line.split('@@@@@@@@@@')
+                # new paragraph
+                category = model.get(title, None)
+                if category == None:
+                    model[title] = [0, 0, {'<sos>': 0}, {}]
+                    category = model[title]
+                category[0] += 1
+                category[1] += 1
+                category[2]['<sos>'] += 1
+                # count words of the paragraph
+                words = text.split(' ')
+                previous_word = '<sos>'
+                for word in words:
+                    if word == ' ' or word == '' or word == '\n':
+                        continue
+                    
+                    category_unary = category[2]
+                    category_unary[word] = 1 if category_unary.get(word, None) == None else (category_unary[word] + 1)
+
+                    binary = previous_word + '-' + word
+                    category_binary = category[3]
+                    category_binary[binary] = 1 if category_binary.get(binary, None) == None else (category_binary[binary] + 1)
+                    
+                    previous_word = word
+        with open('result.json', 'w') as fp:
+            json.dump(model, fp, indent=4)
+        self.model = model
 
 def main():
     # this lines saves all the words and titles in seperate files
@@ -120,7 +155,12 @@ def main():
 
     # this line stores the number of word occurences in each context in a csv file
     # Model.count_words_for_titles('titles.txt', 'words.txt', 'HAM-Train.txt', 'word_count.csv', say=True)
-    pass
+    # a = {1: 'a', 2: 'b'}
+    # print(a.get(3, None))
+
+    x = Model('xxx')
+    x.build_model('HAM-Train.txt', True)
+    # pass
 
     # dict = {'a': [1, 2, 3], 'b': [1, 2, 4]}
     # a = pd.DataFrame.from_dict(dict)
